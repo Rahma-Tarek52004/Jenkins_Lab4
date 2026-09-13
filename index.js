1050ccfc7afe302e4a713f244bf173bb37a8d18d
Rahma-Tarek52004 <rahmatarek52004@gmail.com>
2026-09-13 21:56:40 +0200

    Update greeting message in index.js

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const port = process.env.PORT || 3000;
 
 app.get("/", (req, res) => {
   res.json({
-    message: "Hello from the Node.js app mesh batta khaleeeessssss!",
+    message: "Hello from the Node.js app mesh batta khaleeeessssss maleeeeesssss!",
     nodeVersion: process.version,
     hostname: os.hostname(),
   });
