b7070479e146e6382a40b8f4d08eac7a4ff9f1fb
Rahma-Tarek52004 <rahmatarek52004@gmail.com>
2026-09-13 21:12:00 +0200

    Update index.js

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const port = process.env.PORT || 3000;
 
 app.get("/", (req, res) => {
   res.json({
-    message: "Hello from the Node.js app mesh batta2!",
+    message: "Hello from the Node.js app mesh batta khaleeeessssss!",
     nodeVersion: process.version,
     hostname: os.hostname(),
   });
